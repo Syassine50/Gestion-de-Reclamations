3fb61568cfd5e41059d6a9aa98291cd56dd50326
Syassine50 <yassine.saadouni.77@gmail.com>
2025-05-11 12:44:34 +0100

    fix backend

---
+++ backend_GeRe/src/main/java/tn/saaadouni/yassine/backend_gere/Config/CorsConfig.java
@@ -0,0 +1,22 @@
+package tn.saaadouni.yassine.backend_gere.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+
+                .allowedOrigins("http://localhost:4200", "https://www.espace-client.wassimlephotographe.com", "https://espace-client.wassimlephotographe.com", "http://192.168.1.14:4200") // Replace with your Angular app's origin
+
+               .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization", "Cookie")
+                .exposedHeaders(HttpHeaders.SET_COOKIE)
+                .allowCredentials(true);
+    }
+}
--- backend_GeRe/src/main/java/tn/saaadouni/yassine/backend_gere/Config/SecurityConfig.java
+++ backend_GeRe/src/main/java/tn/saaadouni/yassine/backend_gere/Config/SecurityConfig.java
@@ -1,41 +1,86 @@
 package tn.saaadouni.yassine.backend_gere.Config;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import tn.saaadouni.yassine.backend_gere.repositories.AppUserRepository;
 
 
 @Configuration
+@EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig {
 
-    private final AppUserRepository userRepository;
-
-    public SecurityConfig(AppUserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
 
 
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(Customizer.withDefaults())
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/api/auth/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults()); // Only for testing
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/auth/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:4200"); // Replace with your Angular app's origin
+        config.addAllowedOrigin("http://192.168.1.14:4200"); // Replace with your Angular app's origin
+        config.addAllowedOrigin("http://localhost:21453"); // Replace with your Angular app's origin
+
+
+        config.addAllowedOrigin("https://www.espace-client.wassimlephotographe.com");
+        config.addAllowedOrigin("https://espace-client.wassimlephotographe.com");
+
+
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PATCH");
+        config.addAllowedHeader("Origin");
+        config.addAllowedHeader("X-Requested-With");
+        config.addAllowedHeader("Content-Type");
+        config.addAllowedHeader("Accept");
+        config.addAllowedHeader("Authorization");
+        config.addAllowedHeader("Cookie");
+        config.addExposedHeader(HttpHeaders.SET_COOKIE);
+        config.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
